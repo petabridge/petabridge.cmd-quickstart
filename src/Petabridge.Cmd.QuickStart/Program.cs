b391f8ebf36f02946c236d4ce97f1bd9154c6214
Jose Carlos Marquez <JCMarquez@sportingsolutions.com>
2017-06-26 11:50:16 +0100

    Add Cluster support and configurability

--- src/Petabridge.Cmd.QuickStart/Program.cs
+++ src/Petabridge.Cmd.QuickStart/Program.cs
@@ -3,7 +3,11 @@
 //      Copyright (C) 2017 - 2017 Petabridge, LLC <https://petabridge.com>
 // </copyright>
 // -----------------------------------------------------------------------
+
+using System;
+using System.Configuration;
 using Akka.Actor;
+using Petabridge.Cmd.Cluster;
 using Petabridge.Cmd.Host;
 
 namespace Petabridge.Cmd.QuickStart
@@ -12,10 +16,20 @@ namespace Petabridge.Cmd.QuickStart
     {
         private static void Main(string[] args)
         {
-            using (var actorSys = ActorSystem.Create("MyActorSystem"))
+            var actorSystemName = ConfigurationManager.AppSettings["ActorSystemName"];
+
+            var configuration = AkkaConfiguration.GetAkkaConfig(
+                AkkaConfiguration.Sections.AkkaActor,
+                AkkaConfiguration.Sections.AkkaRemote,
+                AkkaConfiguration.Sections.AkkaCluster,
+                AkkaConfiguration.Sections.AkkaPetabridgeCmd);
+
+            using (var actorSys = ActorSystem.Create(actorSystemName, configuration))
             {
                 var pbm = PetabridgeCmd.Get(actorSys); // creates the Petabridge.Cmd.Host
                 pbm.RegisterCommandPalette(new MsgCommandPaletteHandler()); // register custom command palette
+                pbm.RegisterCommandPalette(ClusterCommands.Instance);
+
                 pbm.Start(); // begins listening for incoming connections on Petabridge.Cmd.Host
 
                 actorSys.WhenTerminated.Wait();
